2d64624d01da5d05d222e753a4829cbfd2aca8ad
Caleb <calebabhulimhen@gmail.com>
2020-05-28 17:30:43 -0400

    Adds various option in navbar to customize webpage based on user preferences

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -15,38 +15,63 @@
 /**
  * Initializes the dropdown menu on the top fixed navbar.
  */
-$(document).ready(function(){
-    $(".dropdown-trigger").dropdown();
+$(document).ready(function () {
+  $(".dropdown-trigger").dropdown();
 });
-
-
-
 
 /**
  * Adds a random fun fact about me on each click.
  */
 
 function randomFunFacts() {
-  const funFacts =
-      ['I"m a  Rising sophomore Computer Science major',
-       'This is my first internship', 
-       'I was raised and high-schooled in Lagos, Nigeria',
-       'I speak three languages: English, and two native lanuages, Yoruba and Efik'];
+  const funFacts = [
+    'I"m a  Rising sophomore Computer Science major',
+    "This is my first internship",
+    "I was raised and high-schooled in Lagos, Nigeria",
+    "I speak three languages: English, and two native lanuages, Yoruba and Efik",
+  ];
 
   // Picking a random fact.
   const myFact = funFacts[Math.floor(Math.random() * funFacts.length)];
 
   // Add it to the page.
-  const factBox = document.getElementById('factBox');
+  const factBox = document.getElementById("factBox");
   factBox.innerHTML = myFact;
 }
 
-var slides = document.querySelectorAll('#slides .image-box');
+var slides = document.querySelectorAll("#slides .image-box");
 var currentSlide = 0;
-var slideInterval = setInterval(nextSlide,3000);
+var slideInterval = setInterval(nextSlide, 3000);
 
 function nextSlide() {
-    slides[currentSlide].className = 'image-box';
-    currentSlide = (currentSlide+1)%slides.length;
-    slides[currentSlide].className = 'image-box current';
+  slides[currentSlide].className = "image-box";
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].className = "image-box current";
+}
+
+function changeColor(color) {
+  color_boxes = document.querySelectorAll("#color-id");
+  for (var i = 0; i < color_boxes.length; i++) {
+    color_boxes[i].style.backgroundColor = color;
+  }
+}
+
+function changeFont(font) {
+  var bodyContainer = document.getElementsByTagName("body");
+  for (var i = 0; i < bodyContainer.length; i++) {
+    bodyContainer[i].style.fontStyle = font;
+  }
+}
+
+function changePattern(pattern) {
+  var bodyContainer = document.getElementsByTagName("body");
+  for (var i = 0; i < bodyContainer.length; i++) {
+    if (pattern == "one") {
+      bodyContainer[i].style.backgroundImage = "url(/images/bg1.png)";
+    } else if (pattern == "two") {
+      bodyContainer[i].style.backgroundImage = "url(/images/bg2.png)";
+    } else if (pattern == "three") {
+      bodyContainer[i].style.backgroundImage = "url(/images/bg3.png)";
+    }
+  }
 }
