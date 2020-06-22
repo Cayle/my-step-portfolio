8ec2bd0b8775a168173e7d802e07b698e27b83ad
Caleb <calebabhulimhen@gmail.com>
2020-06-22 20:47:39 +0000

    Make requested changes based on the PR reviews.

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -119,43 +119,68 @@ public final class FindMeetingQuery {
    * @param events It contains a collection of event objects
    * @param request It contains a MeetingRequest object.
    * @return Collection<TimeRange> contains all the possibly available TimeRanges.
+   * Algorithm:
+   * --> Get a collection of available timerange by iterating through booked times and saving the gap(time) between two consecutive booked timeranges.
+   * --> Derive another collection of available timerange based on the timerange gotten from the previous step, while considering booked times of optional attendees.
+   * --> If the time range from step 2 is empty(meaning that the booked time of each optional attendee conflicts with available times of mandatory attendees,
+   * -->    we ignore it and return the time range from step 1.
+   * --> Else, if we could make a new non-empty timerange, we return that.
    */
   public Collection<TimeRange> getAllTheAvailableTime(List<Event> newEvents, ArrayList<TimeRange> attendeesBookedTimes, MeetingRequest request) {
     ArrayList<TimeRange> availableTimeRanges = new ArrayList<>();
-    Integer eventStartTime = 0;
+    Integer eventEndTime = 0;
     for (int l = 0; l < attendeesBookedTimes.size(); l++) {
         TimeRange bookedTimeRange = attendeesBookedTimes.get(l);
-        if (eventStartTime <  bookedTimeRange.start()) {
-            Integer availableTimeToMeet = bookedTimeRange.start() - eventStartTime;
+        if (eventEndTime <  bookedTimeRange.start()) {
+            Integer availableTimeToMeet = bookedTimeRange.start() - eventEndTime;
             if (availableTimeToMeet >= request.getDuration()) {
-                TimeRange availableTimeToBook = TimeRange.fromStartEnd(eventStartTime, bookedTimeRange.start(), false);
+                TimeRange availableTimeToBook = TimeRange.fromStartEnd(eventEndTime, bookedTimeRange.start(), false);
                 availableTimeRanges.add(availableTimeToBook);
             }
-            eventStartTime = bookedTimeRange.end();
+            eventEndTime = bookedTimeRange.end();
         }
-        else if ( eventStartTime == bookedTimeRange.start()){
-            eventStartTime = bookedTimeRange.end();
+        else if ( eventEndTime == bookedTimeRange.start()){
+            eventEndTime = bookedTimeRange.end();
         }
     }
 
     //adds a final TimeRange if the previous meeting didn't last till the end of the day.
     Integer endOfTheDay = 24 * 60;
-    if (eventStartTime < endOfTheDay) {
-        TimeRange availableTimeToBook = TimeRange.fromStartEnd(eventStartTime, endOfTheDay, false);
+    if (eventEndTime < endOfTheDay) {
+        TimeRange availableTimeToBook = TimeRange.fromStartEnd(eventEndTime, endOfTheDay, false);
         availableTimeRanges.add(availableTimeToBook);
     }
 
+    Collection<TimeRange> availableTimeRangesWithOptional = checkBookedOptionalAttendees(newEvents, availableTimeRanges, request, eventEndTime);
+
+    //confirms if any additional TimeRange was added.
+    if (availableTimeRangesWithOptional.isEmpty()) {
+        return availableTimeRanges;
+    }
+    return availableTimeRangesWithOptional;
+  }
+  
+  /**
+   * This method uses the above helper functions to decide the available time 
+   * based on the booked events and requested duration and attendees.
+   * @param newEvents It contains a collection of event objects
+   * @param availableTimeRanges It contains the available time slots, considering just mandatory attendees.
+   * @param request It contains a MeetingRequest object.
+   * @param eventEndTime saves the end time of the last event.
+   * @return Collection<TimeRange> contains all the time slot available.
+   */
+  public Collection<TimeRange> checkBookedOptionalAttendees(List<Event> newEvents, ArrayList<TimeRange> availableTimeRanges, MeetingRequest request, Integer eventEndTime) {
     Collection<TimeRange> availableTimeRangesWithOptional = new ArrayList<>();
     Set<String> optionalAttendees = new HashSet<>(request.getOptionalAttendees());
-  
-    //check the booked TimeRange of optional attendees to see if they can be fitted into the available TimeRange already derived.
+    Integer endOfTheDay = 24 * 60;
+
     Integer optionalStartTime = 0;
-    for (int j = 0; j < newEvents.size() ; j++)  {
-        Set<String> eventAttendees = newEvents.get(j).getAttendees();
+    for (int eventIndex = 0; eventIndex < newEvents.size() ; eventIndex++)  {
+        Set<String> eventAttendees = newEvents.get(eventIndex).getAttendees();
         if (optionalAttendees.containsAll(eventAttendees)) {
-            TimeRange eventTime = newEvents.get(j).getWhen();
-            for (int i = 0 ; i < availableTimeRanges.size(); i++) {
-                if (availableTimeRanges.get(i).contains(eventTime)) {
+            TimeRange eventTime = newEvents.get(eventIndex).getWhen();
+            for (int timeRangeIndex = 0 ; timeRangeIndex < availableTimeRanges.size(); timeRangeIndex++) {
+                if (availableTimeRanges.get(timeRangeIndex).contains(eventTime)) {
                     if (optionalStartTime < eventTime.start()) {
                         TimeRange newAvailableTime = TimeRange.fromStartEnd(optionalStartTime, eventTime.start(), false);
                         availableTimeRangesWithOptional.add(newAvailableTime);
@@ -165,17 +190,11 @@ public final class FindMeetingQuery {
             }
         }
     }
-    if (optionalStartTime < endOfTheDay) {
-        TimeRange availableTimeToBook = TimeRange.fromStartEnd(eventStartTime, endOfTheDay, false);
+    if (optionalStartTime < endOfTheDay & optionalStartTime != 0) {
+        TimeRange availableTimeToBook = TimeRange.fromStartEnd(eventEndTime, endOfTheDay, false);
         availableTimeRangesWithOptional.add(availableTimeToBook);
     }
-
-    //confirms if any additional TimeRange was added.
-    if (optionalStartTime != 0) {
-        return availableTimeRangesWithOptional;
-    }
-
-    return availableTimeRanges;
+    return availableTimeRangesWithOptional;
   }
 
 
@@ -189,10 +208,8 @@ public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
     long whole_day = 24*60;
 
-    if (request.getAttendees().isEmpty()) {
-        if (request.getOptionalAttendees().isEmpty()) {
+    if (request.getAttendees().isEmpty() & request.getOptionalAttendees().isEmpty()) {
             return Arrays.asList(TimeRange.WHOLE_DAY);
-        }
     }
 
     if (request.getDuration() > whole_day) {
